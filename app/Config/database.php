0f67d52097f77eb2468c715b8f491070b2321d30
vamcart <vamshop@gmail.com>
2026-03-06 00:45:37 +0300

    add

---
+++ app/Config/database.php
@@ -0,0 +1,11 @@
+<?php
+class DATABASE_CONFIG {
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'database',
+		'login' => 'root',
+		'password' => 'tiger',
+		'database' => 'statistics',
+	);
+}
